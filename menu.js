68b148dc06ca832b8699098461b0b7edaadccc6e
tyhang <xarzhi@163.com>
2024-11-01 09:18:48 +0800

    first commit

---
+++ menu.js
@@ -0,0 +1,94 @@
+const menu = [
+	{
+		cate: 'Canvas',
+		content: [
+			{
+				title: '贪吃蛇',
+				path: './canvas/snake.html',
+			},
+			{
+				title: '掉落的小球',
+				path: './canvas/ballFall.html',
+			},
+			{
+				title: '粒子特效',
+				path: './canvas/particles.html',
+			},
+			{
+				title: '两球相撞',
+				path: './canvas/towBallsPengPeng.html',
+			},
+			{
+				title: '时钟',
+				path: './canvas/clock.html',
+			},
+			{
+				title: '颜色拾取',
+				path: './canvas/cllectColor.html',
+			},
+		],
+	},
+	{
+		cate: 'Three.js',
+		content: [
+			{
+				title: '螺旋的环',
+				path: './threejs/rotate-circle.html',
+			},
+		],
+	},
+	{
+		cate: 'Web api',
+		content: [
+			{
+				title: 'moveball',
+				path: './webapi/moveball.html',
+			},
+			{
+				title: '图片懒加载',
+				path: './webapi/imgs-layload/index.html',
+			},
+			{
+				title: '视频暂停',
+				path: './webapi/pauseVideo/index.html',
+			},
+		],
+	},
+	{
+		cate: 'Others',
+		content: [
+			{
+				title: '贪吃蛇Dom版',
+				path: './othres/snake/index.html',
+			},
+			{
+				title: '日历',
+				path: './othres/calendar.html',
+			},
+		],
+	},
+
+	{
+		cate: 'Css',
+		content: [
+			{
+				title: '表单验证',
+				path: './css/表单验证.html',
+			},
+			{
+				title: '动态水滴',
+				path: './css/动态水滴.html',
+			},
+			{
+				title: '表单美化',
+				path: './css/表单美化.html',
+			},
+			{
+				title: '旋转百叶窗',
+				path: './css/旋转百叶窗.html',
+			},
+		],
+	},
+]
+
+export default menu
